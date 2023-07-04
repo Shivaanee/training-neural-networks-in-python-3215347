a2abd0316814b4acec379b50c7b51a9b2f15029f
Shivaanee <89640504+Shivaanee@users.noreply.github.com>
2023-07-04 09:04:13 +0000

    02_08OR

--- src/MLP.py
+++ src/MLP.py
@@ -1,19 +1,20 @@
 import numpy as np
 
+
 class Perceptron:
     """A single neuron with the sigmoid activation function.
        Attributes:
           inputs: The number of inputs in the perceptron, not counting the bias.
           bias:   The bias term. By default it's 1.0."""
 
-    def __init__(self, inputs, bias = 1.0):
-        """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
-        self.weights = (np.random.rand(inputs+1) * 2) - 1 
+    def __init__(self, inputs, bias=1.0):
+        """Return a new Perceptron object with the specified number of inputs (+1 for the bias)."""
+        self.weights = (np.random.rand(inputs+1) * 2) - 1
         self.bias = bias
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        x_sum = np.dot(np.append(x,self.bias),self.weights)
+        x_sum = np.dot(np.append(x, self.bias), self.weights)
         return self.sigmoid(x_sum)
 
     def set_weights(self, w_init):
@@ -25,14 +26,14 @@ class Perceptron:
         return 1/(1+np.exp(-x))
 
 
-#test code
+# test code
 neuron = Perceptron(inputs=2)
-neuron.set_weights([10,10,-15]) #AND
+neuron.set_weights([20, 20, -10])  # OR
 
 # Challenge: Write your OR weights
 
 print("Gate:")
-print ("0 0 = {0:.10f}".format(neuron.run([0,0])))
-print ("0 1 = {0:.10f}".format(neuron.run([0,1])))
-print ("1 0 = {0:.10f}".format(neuron.run([1,0])))
-print ("1 1 = {0:.10f}".format(neuron.run([1,1])))
+print("0 0 = {0:.10f}".format(neuron.run([0, 0])))
+print("0 1 = {0:.10f}".format(neuron.run([0, 1])))
+print("1 0 = {0:.10f}".format(neuron.run([1, 0])))
+print("1 1 = {0:.10f}".format(neuron.run([1, 1])))
